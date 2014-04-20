a6eb57a2482a3aec738a46f3b80a6760e2bbe197
svs <svs@svs.io>
2014-04-20 14:11:28 +0530

    adds routes

---
+++ chess_app.rb
@@ -0,0 +1,14 @@
+require 'action_dispatch'
+
+class Router
+
+  def self.routes
+    ActionDispatch::Routing::RouteSet.new.tap do |r|
+      r.draw do
+        resources :games, :only => [:index, :create, :show, :update]
+      end
+    end
+  end
+
+
+end
