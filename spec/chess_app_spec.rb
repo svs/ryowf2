af725d852f77d1cfbb0c0bb0fcc88a9c82393bee
svs <svs@svs.io>
2014-04-20 14:14:17 +0530

    Adds Rack app specs - failing

--- spec/chess_app_spec.rb
+++ spec/chess_app_spec.rb
@@ -9,4 +9,13 @@ describe Router do
     specify { Router.routes.recognize_path('/games/1').should == {:action=>"show", :controller=>"games", :id => "1"} }
   end
 
+  context "given a Rack env" do
+    let(:index_env) { Rack::MockRequest.env_for('/games', {'REQUEST_METHOD' => 'get'}) }
+
+    specify { Router.new(index_env).should route_to(GamesController::Index) }
+    specify { Router.new(Rack::MockRequest.env_for('/games', {'REQUEST_METHOD' => 'post'})).should route_to(GamesController::Create) }
+    specify { Router.new(Rack::MockRequest.env_for('/games/2/', {'REQUEST_METHOD' => 'PUT'})).should route_to(GamesController::Update).with("id" => "2") }
+  end
+
+
 end
